b106a51398854c9006d7610ebf20a036e01e0851
francescolucco <francescolucco@googlemail.com>
2022-01-19 17:23:58 +0100

    prezzo totale

--- php-oop-2/index.php
+++ php-oop-2/index.php
@@ -3,86 +3,27 @@
 require_once __DIR__ . "/classes/User.php";
 require_once __DIR__ . "/classes/UserPremium.php";
 require_once __DIR__ . "/classes/Product.php";
+require_once __DIR__ . "/classes/showcase.php";
 
 $new_user = new User('Francesco', 'Lucco', 'Via Roma 11 - Verona');
-// var_dump($new_user);
+$new_user->setProductsToBuy($product3, $product6, $product2, $product4);
+
+foreach ($new_user->getProductsToBuy() as $product) {
+   echo "<br>" . $product->name . ": " . $product->price . "&euro;" . "<br>" . "Quntità: " . $product->quantity . "<br>";
+ };
+
+ //$new_user->setTotalPay([$product3]);
+
+//  $new_user->setProductsToBuy($product3);
+ var_dump($new_user);
+
+ echo " Costo totale nel carrello: " . $new_user->getTotalPay() . " &euro;";
+
+
 
 $new_user_premium = new UserPremium('Alessandro', 'Manzoni', 'Via dei Promessi Sposi 600c - Como', 'Premium');
 // var_dump($new_user_premium);
 
-$new_product = new Product('Friggitrice ad Aria Calda', 'Innsky', 132.99);
-$new_product_2 = new Product('Frigorifero Combinato RB7300T Silver Inox', 'Samsung', 529.00);
-$new_product_3 = new Product('Smart TV LED HD 32"', 'Caixun', 1329.99);
-$new_product_4 = new Product('TV QLED', 'Samsung', 819.00);
-$new_product_5 = new Product('iPhone 13 Pro Max', 'Apple', 1639.00);
-$new_product_6 = new Product('aspirapolvere Senza Filo Cyclone V10 Animal', 'Dyson', 404.37);
-// var_dump($new_product);
-
-$new_user_premium->buyProduct($new_product_4);
-$new_user_premium->buyProduct($new_product_6);
-$new_user_premium->getTotalSpending($new_product_4);
-$new_user_premium->getTotalSpending($new_product_6);
-
-
-$new_user->buyProduct($new_product);
-$new_user->buyProduct($new_product_5);
-$new_user->buyProduct($new_product_3);
-$new_user->getTotalSpending($new_product);
-$new_user->getTotalSpending($new_product_5);
 
 
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
-</head>
-
-<body>
-   <div class="user-basic">
-      <h1>UTENTE BASE</h1>
-      <h3>
-         <?php echo $new_user->getName() ?> <?php echo $new_user->getLastname() ?>
-      </h3>
-      <h5>Prodotti comprati dall'utente:
-         <ul>
-            <?php foreach ($new_user->buyProduct($new_product_2) as $product) : ?>
-               <li><?php echo $product ?></li>
-            <?php endforeach; ?>
-         </ul>
-      </h5>
-      <h5> Spesa totale: <br>
-         <strong><?php echo $new_user->getTotalSpending($new_product_2) ?></strong> &euro;
-      </h5>
-      <h5> Punti totalizzati: <br>
-      <strong><?php echo $new_user->getPoints() ?></strong> punti
-   </h5>
-</div>
-<div class="user-premium">
-   <h1>UTENTE PREMIUM</h1>
-   <h3>
-      <?php echo $new_user_premium->getName() ?> <?php echo $new_user_premium->getLastname() ?>
-   </h3>
-   <h5>Prodotti comprati dall'utente <?php echo $new_user_premium->getPackage() ?>:
-   <ul>
-      <?php foreach ($new_user_premium->buyProduct($new_product_2) as $product) : ?>
-         <li><?php echo $product ?></li>
-         <?php endforeach; ?>
-      </ul>
-   </h5>
-   <h5> Spesa totale: <br>
-      <strong><?php echo $new_user_premium->getTotalSpending($new_product_2) ?></strong> &euro;
-   </h5>
-   <h5> Punti totalizzati: <br>
-   <strong><?php echo $new_user_premium->getPoints() ?></strong> punti
-</h5>
-</div>
-</body>
-
-</html>
